a33ac59f3bc6fff8b401929d52ad702c8cce0a6b
Douglas Dreer <douglasdreer@outlook.com.br>
2025-09-03 17:15:42 -0300

    [TEST][FEAT-0001] ServerApplicationTests.kt

---
+++ src/test/kotlin/br/com/babershop/server/ServerApplicationTests.kt
@@ -0,0 +1,13 @@
+package br.com.babershop.server
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class ServerApplicationTests {
+
+    @Test
+    fun contextLoads() {
+    }
+
+}
