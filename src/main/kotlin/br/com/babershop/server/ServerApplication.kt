26f3ad7fd2485ce42378fc822cd8eaaf51bf7762
Douglas Dreer <douglasdreer@outlook.com.br>
2025-09-03 23:58:32 -0300

    FEAT [86abh97n7][86abh9823] Configuração do POM

--- src/main/kotlin/br/com/babershop/server/ServerApplication.kt
+++ src/main/kotlin/br/com/babershop/server/ServerApplication.kt
@@ -2,10 +2,12 @@ package br.com.babershop.server
 
 import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.runApplication
+import org.springframework.cloud.openfeign.EnableFeignClients
 
 @SpringBootApplication
+@EnableFeignClients
 class ServerApplication
+    fun main(args: Array<String>) {
+        runApplication<ServerApplication>(*args)
+    }
 
-fun main(args: Array<String>) {
-    runApplication<ServerApplication>(*args)
-}
